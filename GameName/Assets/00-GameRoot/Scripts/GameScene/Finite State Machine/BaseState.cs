358fd5a9c6f94ca6b8375f1d693c35499be46ac7
TheAidaan <56072883+TheAidaan@users.noreply.github.com>
2021-05-02 13:05:53 +0200

    getting there........
    its due tomorrow :-1:

--- GameName/Assets/00-GameRoot/Scripts/GameScene/Finite State Machine/BaseState.cs
+++
@@ -1,7 +0,0 @@
-
-public abstract class BaseSate<T>
-{
-    public abstract void EnterState(T script);
-    public abstract void IdleState(T script);
-    public abstract void ExitState(T script);
-}
